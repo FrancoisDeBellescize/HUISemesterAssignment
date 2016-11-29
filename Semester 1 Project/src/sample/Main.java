735227d1a8905a33fb3c2cf896b080ccca62376e
François de bellescize <Franois de bellescize>
2016-11-29 17:11:35 +0000

    Vector3 + Generation map

--- Semester 1 Project/src/sample/Main.java
+++ Semester 1 Project/src/sample/Main.java
@@ -5,12 +5,44 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.util.Hashtable;
 
 public class Main extends Application {
 
+    Hashtable board;
+
+    void initGrid() {
+        board = new Hashtable();
+
+        int x = 0;
+        int y = 3;
+        int z = -3;
+
+        int line_size = 4;
+        int size_map = 7;
+
+        for (int j = 0; j < size_map; j++) {
+            for (int i = 0; i < line_size; i++) {
+                board.put(new Vector3(x, y, z), new Case());
+            }
+            if (j < size_map / 2) {
+                x -= 1;
+                line_size += 1;
+            } else {
+                y -= 1;
+                line_size -= 1;
+            }
+            z += 1;
+        }
+    }
+
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        initGrid();
+
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
